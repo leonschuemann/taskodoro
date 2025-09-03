7f0e6c25f5b55b2a494e621615b6926fcd0af393
Leon Schümann <leon-schuemann@gmx.net>
2025-09-04 00:11:20 +0200

    Simple settings for pomodoro timer using the shared preferences package

---
+++ lib/constants/default_settings.dart
@@ -0,0 +1,8 @@
+class DefaultSettings {
+  static const int focusTime = 25;
+  static const int longBreakTime = 5;
+  static const int shortBreakTime = 15;
+  static const int repetitions = 4;
+  static const int minRepetitions = 1;
+  static const int maxRepetitions = 12;
+}
--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:taskodoro/constants/default_settings.dart';
+import 'package:taskodoro/themes/spacing_theme.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -8,22 +12,107 @@ class SettingsPage extends StatefulWidget {
 }
 
 class SettingsPageState extends State<SettingsPage> {
+  late final SharedPreferences _sharedPreferences;
+  final TextEditingController _focusTimerController = TextEditingController();
+  final TextEditingController _shortBreakTimerController = TextEditingController();
+  final TextEditingController _longBreakTimerController = TextEditingController();
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    _initializeSharedPreferences();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _focusTimerController.dispose();
+    _shortBreakTimerController.dispose();
+    _longBreakTimerController.dispose();
+    super.dispose();
+  }
+  
   @override
   Widget build(BuildContext context) {
+    const double textFieldWidth = 56;
+    int repetitions;
+
+    if (!_isLoading && _sharedPreferences.getInt('repetitions') != null) {
+      repetitions = _sharedPreferences.getInt('repetitions')!;
+    } else {
+      repetitions = DefaultSettings.repetitions;
+    }
+
+    final List<Widget> settings = <Widget>[
+      Text(
+        'Pomodoro Timer',
+        style: Theme.of(context).textTheme.headlineSmall,
+      ),
+      textInputSetting(
+        text: 'Focus Timer',
+        textFieldWidth: textFieldWidth,
+        onTextFieldChanged: (String value) async {
+          await _sharedPreferences.setInt('focusTime', int.parse(value));
+        },
+        controller: _focusTimerController,
+        maxInputLength: 3,
+      ),
+      textInputSetting(
+        text: 'Long Break Timer',
+        textFieldWidth: textFieldWidth,
+        onTextFieldChanged: (String value) async {
+          await _sharedPreferences.setInt('longBreakTime', int.parse(value));
+        },
+        controller: _longBreakTimerController,
+        maxInputLength: 3,
+      ),
+      textInputSetting(
+        text: 'Short Break Timer',
+        textFieldWidth: textFieldWidth,
+        onTextFieldChanged: (String value) async {
+          await _sharedPreferences.setInt('shortBreakTime', int.parse(value));
+        },
+        controller: _shortBreakTimerController,
+        maxInputLength: 3,
+      ),
+      Row(
+        children: <Widget>[
+          Text('Amount of repetitions'),
+          const Spacer(),
+          Text(DefaultSettings.minRepetitions.toString()),
+          Slider(
+            value: repetitions.toDouble(),
+            onChanged: (double value) async {
+              _sharedPreferences.setInt('repetitions', value.toInt());
+
+              setState(() {
+                repetitions = value.toInt();
+              });
+            },
+            min: DefaultSettings.minRepetitions.toDouble(),
+            max: DefaultSettings.maxRepetitions.toDouble(),
+            divisions: DefaultSettings.maxRepetitions - DefaultSettings.minRepetitions,
+            label: repetitions.toString(),
+          ),
+          Text(DefaultSettings.maxRepetitions.toString()),
+        ],
+      )
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Settings"),
+        title: Text('Settings'),
         backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Row(
-          children: [
+          children: <Widget>[
             Expanded(
               child: FractionallySizedBox(
                 child: Column(
-                  children: [
-                    Text("Sections"),
+                  children: <Widget>[
+                    Text('Sections'),
                   ],
                 )
               ),
@@ -32,16 +121,66 @@ class SettingsPageState extends State<SettingsPage> {
             Expanded(
               flex: 4,
               child: FractionallySizedBox(
-                child: Column(
-                  children: [
-                    Text("Setting")
-                  ],
-                ),
+                widthFactor: 0.5,
+                child: ListView.separated(
+                    itemBuilder: (BuildContext context, int index) {
+                      return settings[index];
+                    },
+                    separatorBuilder: (BuildContext context, int index) {
+                      return const SizedBox(height: SpacingTheme.smallGap);
+                    },
+                    itemCount: settings.length,
+                )
               ),
             )
           ]
         ),
       ),
+    );
+  }
+  
+  Future<void> _initializeSharedPreferences() async {
+    _sharedPreferences = await SharedPreferences.getInstance(); // TODO: exchange with own YAML file
+    final int focusTime = _sharedPreferences.getInt('focusTime') ?? DefaultSettings.focusTime;
+    final int longBreakTime = _sharedPreferences.getInt('longBreakTime') ?? DefaultSettings.longBreakTime;
+    final int shortBreakTime = _sharedPreferences.getInt('shortBreakTime') ?? DefaultSettings.shortBreakTime;
+
+    setState(() {
+      _focusTimerController.text = focusTime.toString();
+      _longBreakTimerController.text = longBreakTime.toString();
+      _shortBreakTimerController.text = shortBreakTime.toString();
+      _isLoading = false;
+    });
+  }
+
+  Widget textInputSetting({
+    required String text,
+    required double textFieldWidth,
+    required ValueChanged<String> onTextFieldChanged,
+    required TextEditingController controller,
+    required int maxInputLength
+  }) {
+    return Row(
+      children: <Widget>[
+        Text(text),
+        const Spacer(),
+        SizedBox(
+          width: textFieldWidth,
+          child: TextField(
+            onChanged: onTextFieldChanged,
+            controller: controller,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: SpacingTheme.roundedRectangleBorderRadius,
+              ),
+              isDense: true,
+            ),
+            inputFormatters: <TextInputFormatter>[
+              LengthLimitingTextInputFormatter(maxInputLength),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
