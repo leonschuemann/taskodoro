1ab3c06936158975d9a624b6f5197ee7314666b7
Leon Schümann <leon-schuemann@gmx.net>
2025-09-06 00:11:23 +0200

    Added break cycles to the pomodoro timer and it now also uses the configured focus/break durations

--- lib/screens/pomodoro_page.dart
+++ lib/screens/pomodoro_page.dart
@@ -1,9 +1,13 @@
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:taskodoro/constants/default_settings.dart';
 import 'package:taskodoro/l10n/app_localizations.dart';
 
 class PomodoroPage extends StatefulWidget {
-  const PomodoroPage({super.key});
+  final SharedPreferences sharedPreferences;
+
+  const PomodoroPage({super.key, required this.sharedPreferences});
 
   @override
   State<StatefulWidget> createState() => _PomodoroPageState();
@@ -11,12 +15,22 @@ class PomodoroPage extends StatefulWidget {
 
 class _PomodoroPageState extends State<PomodoroPage> {
   final CountDownController countDownController = CountDownController();
+  late final SharedPreferences sharedPreferences;
+  late int repetitionCount = 1;
   bool isTimerRunning = false;
   bool hasTimerBeenStarted = false;
+  bool isInPause = false;
+
+  @override
+  void initState() {
+    sharedPreferences = widget.sharedPreferences;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     final AppLocalizations localizations = AppLocalizations.of(context)!;
+    final int focusTime = sharedPreferences.getInt('focusTime') ?? DefaultSettings.focusTime;
 
     return Scaffold(
       appBar: AppBar(
@@ -39,7 +53,7 @@ class _PomodoroPageState extends State<PomodoroPage> {
                           CircularCountDownTimer(
                             width: MediaQuery.of(context).size.width / 2,
                             height: MediaQuery.of(context).size.height / 2,
-                            duration: 25 * 60,
+                            duration: focusTime * 60,
                             fillColor: Theme.of(context).colorScheme.primary,
                             ringColor: Theme.of(context).colorScheme.primary,
                             controller: countDownController,
@@ -47,53 +61,24 @@ class _PomodoroPageState extends State<PomodoroPage> {
                             textStyle: Theme.of(context).textTheme.bodyLarge,
                             textFormat: CountdownTextFormat.MM_SS,
                             isReverse: true,
+                            onComplete: resetTimerWithNewTime,
                           ),
                           const SizedBox(height: 50,),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: <Widget>[
                               IconButton(
-                                onPressed: () {
-                                  countDownController.reset();
-
-                                  setState(() {
-                                    isTimerRunning = false;
-                                    hasTimerBeenStarted = false;
-                                  });
-                                },
+                                onPressed: resetTimer,
                                 icon: const Icon(Icons.skip_previous_rounded),
                               ),
                               IconButton(
-                                onPressed: () {
-                                  if (isTimerRunning) {
-                                    countDownController.pause();
-                                  } else if (!hasTimerBeenStarted) {
-                                    countDownController.start();
-
-                                    setState(() {
-                                      hasTimerBeenStarted = true;
-                                    });
-                                  } else {
-                                    countDownController.resume();
-                                  }
-
-                                  setState(() {
-                                    isTimerRunning = !isTimerRunning;
-                                  });
-                                },
+                                onPressed: playResumePauseTimer,
                                 icon: isTimerRunning
                                     ? const Icon(Icons.pause_rounded)
                                     : const Icon(Icons.play_arrow_rounded)
                               ),
                               IconButton(
-                                onPressed: () {
-                                  countDownController.reset();
-
-                                  setState(() {
-                                    isTimerRunning = false;
-                                    hasTimerBeenStarted = false;
-                                  });
-                                },
+                                onPressed: resetTimerWithNewTime,
                                 icon: const Icon(Icons.skip_next_rounded),
                               ),
                             ],
@@ -112,5 +97,71 @@ class _PomodoroPageState extends State<PomodoroPage> {
         ],
       ),
     );
+  }
+
+  void playResumePauseTimer() {
+    if (isTimerRunning) {
+      countDownController.pause();
+    } else if (!hasTimerBeenStarted) {
+      countDownController.start();
+    
+      setState(() {
+        hasTimerBeenStarted = true;
+      });
+    } else {
+      countDownController.resume();
+    }
+    
+    setState(() {
+      isTimerRunning = !isTimerRunning;
+    });
+  }
+
+  int getNextTime() {
+    final int repetitionSetting = sharedPreferences.getInt('repetitions') ?? DefaultSettings.repetitions;
+    int nextTime;
+
+    if (!isInPause) {
+      if (repetitionCount == repetitionSetting) {
+        nextTime = sharedPreferences.getInt('longBreakTime') ?? DefaultSettings.longBreakTime;
+      } else {
+        nextTime = sharedPreferences.getInt('shortBreakTime') ?? DefaultSettings.shortBreakTime;
+      }
+    } else {
+      nextTime = sharedPreferences.getInt('focusTime') ?? DefaultSettings.focusTime;
+    }
+
+    return nextTime * 60;
+  }
+
+  void resetTimerWithNewTime() {
+    final int repetitionSetting = sharedPreferences.getInt('repetitions') ?? DefaultSettings.repetitions;
+    countDownController.restart(duration: getNextTime());
+    countDownController.pause();
+    int newRepetitions = repetitionCount;
+
+    if (!isInPause) {
+      newRepetitions += 1;
+    }
+
+    if (newRepetitions == repetitionSetting + 1) {
+      newRepetitions = 1;
+    }
+
+    setState(() {
+      repetitionCount = newRepetitions;
+      isInPause = !isInPause;
+      isTimerRunning = false;
+      hasTimerBeenStarted = false;
+    });
+  }
+
+  void resetTimer() {
+    countDownController.reset();
+
+    setState(() {
+      isTimerRunning = false;
+      hasTimerBeenStarted = false;
+    });
   }
 }
