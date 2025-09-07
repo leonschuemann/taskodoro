c7f2e21f5bc629f253e387ad54ad7662f74dde11
Leon Schümann <leon-schuemann@gmx.net>
2025-09-07 18:54:22 +0200

    Fix error in test with the newly required SharedPreferences parameter in MyApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:taskodoro/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    final SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp(sharedPreferences: sharedPreferences,));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
